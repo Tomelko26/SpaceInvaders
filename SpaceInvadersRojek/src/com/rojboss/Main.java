4efbac2aedd5407365bbf1755e0d0ff4b3e5d5dd
Rojek T <tomaszrojek26@gmail.com>
2019-01-12 15:24:23 +0100

    ready to play

---
+++ SpaceInvadersRojek/src/com/rojboss/Main.java
@@ -0,0 +1,15 @@
+package com.rojboss;
+
+public class Main {
+
+	public static void main (String argv[]) {
+		Game g = new Game();
+		
+		// Start the main game loop
+		// this method will not return until the game has finished running
+		//
+		g.gameLoop();
+	}
+	
+}
+
